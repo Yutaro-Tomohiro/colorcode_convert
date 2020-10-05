92fe469cd97ffce1c6d6f36f271a16111e63dd09
tomohiro <tomohiro@fenrir.co.jp>
2020-10-05 14:25:17 +0900

    add validations

--- lib/colorcode_convert_rgb/cli.rb
+++ lib/colorcode_convert_rgb/cli.rb
@@ -4,7 +4,14 @@ require "thor"
 module ColorcodeConvertRgb
   class CLI < Thor
     desc "to_ints {Hexagonal_color_code}", "Converts a hexadecimal color code to a decimal number."
-    def to_ints(hex)
+    def to_ints(color)
+      if color.match(/[a-fA-F0-9]{6}/)
+        hex = color
+      elsif color.match(/[#][a-fA-F0-9]{6}/)
+        hex = color.delete("#")
+      else
+        return puts 'Please enter the correct code.(e.g. #ffffff or ffffff )'
+      end
       r, g, b = hex[0..1], hex[2..3], hex[4..5]
       rgb = [r, g, b].map(&:hex)
       puts "RGB is #{rgb}."
